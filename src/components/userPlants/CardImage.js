a0e89d51c93cbae2958305b7f6a790d5bc6fddf3
emileypalmquist <emiley.palmquist@icloud.com>
2021-09-09 21:18:20 -0600

    changed image display url

--- src/components/userPlants/CardImage.js
+++ src/components/userPlants/CardImage.js
@@ -1,16 +1,12 @@
-import React from "react"
-
-const CardImage = ({photo}) => {
+import React from "react";
+const API = process.env.REACT_APP_BACKEND_BASE_URL;
+const CardImage = ({ photo }) => {
   return (
     <>
       {photo.includes("https://") ? (
         <img src={photo} alt="plant" className="plant-card-image" />
       ) : (
-        <img
-          src={`http://localhost:3000${photo}`}
-          alt="plant"
-          className="plant-card-image"
-        />
+        <img src={`${API}${photo}`} alt="plant" className="plant-card-image" />
       )}
     </>
   );
