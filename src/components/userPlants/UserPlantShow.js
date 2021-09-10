a0a9df38fc5e4f4977bec16bc064d532007cc917
emileypalmquist <emiley.palmquist@icloud.com>
2021-09-09 21:27:31 -0600

    fixed plant show page image base url

--- src/components/userPlants/UserPlantShow.js
+++ src/components/userPlants/UserPlantShow.js
@@ -1,92 +1,133 @@
-import {useEffect, useState} from  "react";
-import {connect} from "react-redux";
-import {Link} from "react-router-dom"
-import {Card, Icon, Image, Button} from "semantic-ui-react"
-import {setUserPlantShow, addUserPlantLike, removeUserPlantLike} from "../../redux/actions/plantActions"
-import {api} from "../../services/api"
-import CareNotes from "./CareNotes"
-import Comments from "./Comments"
+import { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { Card, Icon, Image, Button } from "semantic-ui-react";
 
-const UserPlantShow = ({userId, userPlants, showPlant, match, setUserPlantShow, addUserPlantLike, removeUserPlantLike}) => {
+import {
+  setUserPlantShow,
+  addUserPlantLike,
+  removeUserPlantLike,
+} from "../../redux/actions/plantActions";
+import { api } from "../../services/api";
+import CareNotes from "./CareNotes";
+import Comments from "./Comments";
 
-    const handleResponse = (resp) => {
-        if (!resp.error) {
-            setUserPlantShow(resp)
-        }
+const API = process.env.REACT_APP_BACKEND_IMAGE_BASE_URL;
+
+const UserPlantShow = ({
+  userId,
+  userPlants,
+  showPlant,
+  match,
+  setUserPlantShow,
+  addUserPlantLike,
+  removeUserPlantLike,
+}) => {
+  const handleResponse = (resp) => {
+    if (!resp.error) {
+      setUserPlantShow(resp);
     }
+  };
 
-    useEffect(() => {
-        const found = userPlants.filter(p => p.id === parseInt(match.params.id))
-     
-        if (found.length) {
-            setUserPlantShow(found[0])
-          
-        } else {
-          api.userPlants.getUserPlant(match.params.id).then(resp => handleResponse(resp))
-        }
-    }, [userPlants])
+  useEffect(() => {
+    const found = userPlants.filter((p) => p.id === parseInt(match.params.id));
 
-    const handleLike = () => {
-        const like = {user_id: userId, likeable_id: showPlant?.id, likeable_type: "UserPlant"}
-        api.likes.createLike(like)
-        .then(data => addUserPlantLike(data))
+    if (found.length) {
+      setUserPlantShow(found[0]);
+    } else {
+      api.userPlants
+        .getUserPlant(match.params.id)
+        .then((resp) => handleResponse(resp));
     }
+  }, [userPlants]);
 
-    const handleUnLike = (like) => {
-        api.likes.deleteLike(like.id)
-            .then(data => data.message && removeUserPlantLike(like))
-    }
+  const handleLike = () => {
+    const like = {
+      user_id: userId,
+      likeable_id: showPlant?.id,
+      likeable_type: "UserPlant",
+    };
+    api.likes.createLike(like).then((data) => addUserPlantLike(data));
+  };
 
-    const like = showPlant?.likes?.find(like => like.user_id === userId) 
-    return (
-        <div className="plant-show-container">
-            { Object.keys(showPlant).length ? 
-            <>
-            <Card>
+  const handleUnLike = (like) => {
+    api.likes
+      .deleteLike(like.id)
+      .then((data) => data.message && removeUserPlantLike(like));
+  };
+
+  const like = showPlant?.likes?.find((like) => like.user_id === userId);
+  return (
+    <div className="plant-show-container">
+      {Object.keys(showPlant).length ? (
+        <>
+          <Card>
             {showPlant?.photo.includes("https://") ? (
-                <Image src={showPlant?.photo} wrapped ui={false} />
-                ) : (
-                <Image src={`http://localhost:3000${showPlant?.photo}`} wrapped ui={false} />
+              <Image src={showPlant?.photo} wrapped ui={false} />
+            ) : (
+              <Image src={`${API}${showPlant?.photo}`} wrapped ui={false} />
             )}
-                <Card.Content>
-                <Card.Header>{ showPlant?.name }</Card.Header>
-                <Card.Meta>
-                    <span className='date'>{ showPlant?.plant?.name }</span>
-                </Card.Meta>
-                <Card.Description>
-                    difficulty: { showPlant?.difficulty } <br/>
-                    moisture: { showPlant?.moisture } <br/>
-                    {showPlant?.indoor ? "indoor" : "outdoor"}
-                </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    <Link to={`/greenhouse/${showPlant?.user_id}`}><Button>Check out my greenhouse</Button></Link><br/>
-                    
-                    {showPlant?.likes?.length} Likes
-                    {like  ? 
-                    <Icon name='thumbs up' id="like-icon" onClick={() => handleUnLike(like)} /> :
-                    <Icon name='thumbs up outline' id="like-icon" onClick={handleLike} />}
-                   
-                </Card.Content>
-            </Card>
-            <div>
-                <CareNotes careNotes={showPlant?.care_notes} userPlantId={showPlant?.id} plantUserId={showPlant?.user_id}/> 
-                <Comments comments={showPlant?.comments} userPlantId={showPlant?.id} /> 
-            </div>
-            </>
-                :
-                <h1>No Plant Found</h1>
-            }
-        </div>
-    )
-}
+            <Card.Content>
+              <Card.Header>{showPlant?.name}</Card.Header>
+              <Card.Meta>
+                <span className="date">{showPlant?.plant?.name}</span>
+              </Card.Meta>
+              <Card.Description>
+                difficulty: {showPlant?.difficulty} <br />
+                moisture: {showPlant?.moisture} <br />
+                {showPlant?.indoor ? "indoor" : "outdoor"}
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <Link to={`/greenhouse/${showPlant?.user_id}`}>
+                <Button>Check out my greenhouse</Button>
+              </Link>
+              <br />
+              {showPlant?.likes?.length} Likes
+              {like ? (
+                <Icon
+                  name="thumbs up"
+                  id="like-icon"
+                  onClick={() => handleUnLike(like)}
+                />
+              ) : (
+                <Icon
+                  name="thumbs up outline"
+                  id="like-icon"
+                  onClick={handleLike}
+                />
+              )}
+            </Card.Content>
+          </Card>
+          <div>
+            <CareNotes
+              careNotes={showPlant?.care_notes}
+              userPlantId={showPlant?.id}
+              plantUserId={showPlant?.user_id}
+            />
+            <Comments
+              comments={showPlant?.comments}
+              userPlantId={showPlant?.id}
+            />
+          </div>
+        </>
+      ) : (
+        <h1>No Plant Found</h1>
+      )}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
-    return {
-        showPlant: state.plantReducer.userPlantShow,
-        userPlants: state.plantReducer.userPlants,
-        userId: state.userReducer.id
-    }
-}
+  return {
+    showPlant: state.plantReducer.userPlantShow,
+    userPlants: state.plantReducer.userPlants,
+    userId: state.userReducer.id,
+  };
+};
 
-export default connect(mapStateToProps, {setUserPlantShow, addUserPlantLike, removeUserPlantLike})(UserPlantShow);
+export default connect(mapStateToProps, {
+  setUserPlantShow,
+  addUserPlantLike,
+  removeUserPlantLike,
+})(UserPlantShow);
